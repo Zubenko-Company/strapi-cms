fb11a9c7e85103e240fa274ad14dde8be3a21da7
a000011 <anton.nadikin@gmail.com>
2025-10-20 01:41:26 +0400

    add docker

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -4,7 +4,7 @@ export default (config) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     server: {
-      allowedHosts: [config.env("ADMIN_JWT_SECRET")],
+      allowedHosts: ["dimarodin.duckdns.org"],
     },
   });
 };
